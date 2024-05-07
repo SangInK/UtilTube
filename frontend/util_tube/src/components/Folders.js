a7c9208f70044c34c0254193b5a7a279db41fe3e
KANAI\kanai <sai6272.ngn@gmail.com>
2024-05-07 20:29:02 +0900

    ++ frontend 1. footer 배경색 추가 2. Main.js 수정  >> 기존 props를 통해 전달하던 datas, setDatas 등의 status를 context로 처리하도록 수정 (진행중)

--- frontend/util_tube/src/components/Folders.js
+++ frontend/util_tube/src/components/Folders.js
@@ -1,4 +1,6 @@
-import { memo, useEffect, useState } from "react";
+import { memo, useState } from "react";
+
+import { useMain } from "./Main";
 import { useUtil } from "../providers/UtilContext";
 
 import styles from "./Folders.module.css";
@@ -9,7 +11,7 @@ import deleteIcon from "../assets/Folder/icons8-delete-folder-30.png";
 import moveIcon from "../assets/Folder/icons8-move-stock-30.png";
 import cancelIcon from "../assets/Folder/icons8-cancel-30.png";
 
-const Folder = ({ className, folder, subs, onClick, onClickDelete, mode }) => {
+const Folder2 = ({ className, folder, subs, onClick, onClickDelete, mode }) => {
   const folderType = folder.folder_name
     ? "folder"
     : folder.icon === addIcon
@@ -92,7 +94,7 @@ const Folder = ({ className, folder, subs, onClick, onClickDelete, mode }) => {
   );
 };
 
-const FolderForm = ({ className, onClickCancel, onSubmit }) => {
+const FolderForm2 = ({ className, onClickCancel, onSubmit }) => {
   const [folderName, setFolderName] = useState("");
   const { isRunning } = useUtil();
 
@@ -148,7 +150,232 @@ const FolderForm = ({ className, onClickCancel, onSubmit }) => {
   );
 };
 
-const Folders = memo(({ className, datas, setDatas, mode, setMode }) => {
+const Folder = ({ className, folder, subs }) => {
+  const [isMouseOver, setIsMouseOver] = useState(false);
+  const { setDatas, mode, setMode, deleteFolder, selectSubs } = useMain();
+  const { setIsRunning } = useUtil();
+
+  const folderType = folder.folder_name
+    ? "folder"
+    : folder.icon === addIcon
+    ? "plus"
+    : folder.icon === moveIcon || folder.icon === cancelIcon
+    ? "move"
+    : "all";
+
+  const handleClickFolder = async (e) => {
+    e.preventDefault();
+    setIsRunning(true);
+
+    if (isMouseOver) return;
+
+    if (mode === "read") {
+      if (folderType === "folder") {
+        setDatas((current) => ({
+          ...current,
+          currentFolder: folder.id,
+          currentSubs: [...subs],
+        }));
+      } else if (folderType === "plus") {
+        setMode((current) => (current === "read" ? "create" : "read"));
+      } else if (folderType === "all") {
+        const subs = await selectSubs();
+
+        setDatas((current) => ({
+          ...current,
+          subs: [...subs],
+          currentFolder: 0,
+          currentSubs: [...subs],
+        }));
+      } else if (folderType === "move") {
+        setMode((current) => (current === "read" ? "move" : "read"));
+      }
+    } else if (mode === "move") {
+      if (folderType === "folder") {
+        console.log("folder");
+      } else if (folderType === "move") {
+        setMode((current) => (current === "read" ? "move" : "read"));
+      }
+    }
+    setIsRunning(false);
+  };
+
+  const handleClickDelete = async (e) => {
+    setIsRunning(true);
+
+    const folderId = folder.id;
+
+    await deleteFolder(folderId);
+
+    setDatas((current) => {
+      const deleteIndex = current.folders.findIndex(
+        (item) => item.id === folderId
+      );
+
+      const checkCurrentFolder =
+        folderId !== 0 && current.currentFolder !== folderId;
+
+      return {
+        ...current,
+        folders: [
+          ...current.folders.slice(0, deleteIndex),
+          ...current.folders.slice(deleteIndex + 1),
+        ],
+        currentFolder: checkCurrentFolder ? current.currentFolder : 0,
+        currentSubs: checkCurrentFolder ? current.currentSubs : current.subs,
+      };
+    });
+    setIsRunning(false);
+  };
+
+  const handleMouseEvent = (e) => {
+    setIsMouseOver(e.type === "mouseover" ? true : false);
+  };
+
+  return (
+    <div
+      className={className}
+      title={folderType !== "folder" ? folder.alt : folder.folder_name}
+      onClick={handleClickFolder}
+    >
+      {folderType === "folder" ? (
+        <button
+          title="폴더 삭제"
+          onClick={handleClickDelete}
+          onMouseOver={handleMouseEvent}
+          onMouseLeave={handleMouseEvent}
+        >
+          <img src={deleteIcon} alt="폴더 삭제" />
+        </button>
+      ) : null}
+
+      {folderType === "folder" ? (
+        <p className={styles.folderName}>{folder.folder_name}</p>
+      ) : (
+        <img src={folder.icon} alt={folder.alt} />
+      )}
+
+      {folderType === "folder" || folderType === "all" ? (
+        <p className={styles.folderInfo}>
+          {folderType === "all" && "총"}채널수: {subs.length}
+        </p>
+      ) : null}
+    </div>
+  );
+};
+
+const FolderForm = ({ className }) => {
+  const [folderName, setFolderName] = useState("");
+
+  const { setMode, setDatas, createFolder } = useMain();
+  const { isRunning, setIsRunning } = useUtil();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsRunning(true);
+
+    if (isRunning || folderName === "") return;
+
+    const data = {
+      folder_name: folderName,
+    };
+
+    const result = await createFolder(data);
+
+    setDatas((current) => ({
+      ...current,
+      folders: [...current.folders, result],
+    }));
+    setFolderName("");
+
+    setIsRunning(false);
+  };
+
+  const handleClickCancel = () => {
+    setMode((current) => (current === "read" ? "craete" : "read"));
+  };
+
+  /**
+   *
+   * @param {*} e evnet 객체
+   */
+  const handleChange = (e) => {
+    setFolderName(e.target.value);
+  };
+
+  return (
+    <form className={className} onSubmit={handleSubmit}>
+      <input
+        name="folderName"
+        className={styles.input}
+        onChange={handleChange}
+        value={folderName}
+      />
+      <div>
+        <button type="submit">생성</button>
+        <button type="button" onClick={handleClickCancel}>
+          취소
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const Folders = memo(({ className }) => {
+  {
+    const { createStyleClass } = useUtil();
+    const { datas, setDatas, mode, setMode } = useMain();
+
+    return (
+      <div className={createStyleClass(styles, ["folders"], className)}>
+        {mode === "read" || mode === "move" ? (
+          <div className={styles.buttonDiv}>
+            <Folder
+              className={`${styles.folderButton} ${
+                mode === "move" ? styles.disabled : ""
+              }`}
+              folder={{ icon: addIcon, alt: "폴더 추가" }}
+            />
+            <Folder
+              className={styles.folderButton}
+              folder={{
+                icon: mode === "read" ? moveIcon : cancelIcon,
+                alt: mode === "read" ? "채널 이동 모드" : "취소",
+              }}
+            />
+          </div>
+        ) : (
+          <FolderForm className={styles.folderForm} />
+        )}
+
+        <Folder
+          className={`${styles.folder} ${styles.small} ${
+            mode === "move" ? styles.disabled : ""
+          }`}
+          folder={{ icon: allIcon, alt: "전체 폴더" }}
+          subs={datas.subs}
+        />
+
+        {datas.folders.map((folder) => {
+          const subs = datas.subs.filter(
+            (sub) => sub?.folder?.id === folder.id
+          );
+
+          return (
+            <Folder
+              key={folder.id}
+              className={styles.folder}
+              folder={folder}
+              subs={subs}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+});
+
+const Folders2 = memo(({ className, datas, setDatas, mode, setMode }) => {
   const { createStyleClass, executeFetch, setIsRunning } = useUtil();
 
   /**
--- frontend/util_tube/src/components/Main.js
+++ frontend/util_tube/src/components/Main.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, createContext, useContext } from "react";
+
 import { useUtil } from "../providers/UtilContext";
 
 import styles from "./Main.module.css";
@@ -7,7 +8,127 @@ import Folders from "./Folders";
 import Subs from "./Subs";
 import Footer from "./Footer";
 
+const MainContext = createContext({
+  datas: {},
+  setDatas: () => {},
+  mode: "read",
+  setMode: () => {},
+  selectFolder: () => {},
+  createFolder: () => {},
+  deleteFolder: () => {},
+  selectSubs: () => {},
+});
+
+const MainProvider = ({ children }) => {
+  const { executeFetch } = useUtil();
+  const [datas, setDatas] = useState({
+    folders: [],
+    subs: [],
+    currentFolder: 0,
+    currentSubs: [],
+  });
+
+  const [mode, setMode] = useState("read");
+
+  const selectFolder = async () => {
+    const folders = await executeFetch({
+      method: "GET",
+      path: "subs/folders/",
+    });
+
+    return folders;
+  };
+
+  const deleteFolder = async (folderId) => {
+    await executeFetch({
+      method: "DELETE",
+      path: `subs/folder/${folderId}`,
+    });
+  };
+
+  const createFolder = async (data) => {
+    const result = await executeFetch({
+      method: "POST",
+      path: "subs/folders/",
+      data: data,
+    });
+
+    return result;
+  };
+
+  const selectSubs = async (folderId = 0) => {
+    const subs = await executeFetch({
+      method: "GET",
+      path: `subs/${folderId}`,
+    });
+
+    return subs;
+  };
+
+  return (
+    <MainContext.Provider
+      value={{
+        datas,
+        setDatas,
+        mode,
+        setMode,
+        selectFolder,
+        createFolder,
+        deleteFolder,
+        selectSubs,
+      }}
+    >
+      {children}
+    </MainContext.Provider>
+  );
+};
+
+export const useMain = () => {
+  try {
+    const context = useContext(MainContext);
+
+    return context;
+  } catch (error) {
+    console.error(`fialed useContext :: ${error}`);
+  }
+};
+
 const Main = ({ className }) => {
+  const { executeFetch, createStyleClass, setIsRunning } = useUtil();
+  const { setDatas, mode, selectFolder, selectSubs } = useMain();
+
+  useEffect(() => {
+    setIsRunning(true);
+
+    const getData = async () => {
+      try {
+        const folders = await selectFolder();
+
+        const subs = await selectSubs();
+
+        setDatas({ folders, subs, currentSubs: subs, currentFolder: 0 });
+      } catch (e) {
+      } finally {
+        setIsRunning(false);
+      }
+    };
+
+    getData();
+  }, [executeFetch]);
+
+  return (
+    <>
+      <div className={createStyleClass(styles, ["main"], className)}>
+        <Folders className={styles.folders} />
+
+        {mode === "move" ? <h3>move</h3> : <Subs className={styles.subs} />}
+      </div>
+      <Footer className={styles.footer} />
+    </>
+  );
+};
+
+const Main2 = ({ className }) => {
   const { executeFetch, createStyleClass, setIsRunning } = useUtil();
 
   const [datas, setDatas] = useState({
@@ -66,4 +187,8 @@ const Main = ({ className }) => {
   );
 };
 
-export default Main;
+export default ({ className }) => (
+  <MainProvider>
+    <Main className={className} />
+  </MainProvider>
+);
